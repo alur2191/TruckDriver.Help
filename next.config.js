b855eec254dd9e34078420da0d48a819a27d9ac6
dandavisjs <dandavisjs@protonmail.com>
2021-12-22 10:26:07 -0800

    coming soon page:paragraphs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   reactStrictMode: true,
-  // redirects() {
-  //   return [
-  //     process.env.MAINTENANCE_MODE === "1"
-  //       ? { source: "/((?!maintenance|_next).*)", destination: "/comingsoon", permanent: false }
-  //       : null,
-  //   ].filter(Boolean);
-  // }
+  redirects() {
+    return [
+      process.env.MAINTENANCE_MODE === "1"
+        ? { source: "/((?!maintenance|_next).*)", destination: "/comingsoon", permanent: false }
+        : null,
+    ].filter(Boolean);
+  }
 }
