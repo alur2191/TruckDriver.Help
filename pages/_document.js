5b5db40888b55cdc572a9f4d75d296815ee0ab62
dandavisjs <dandavisjs@protonmail.com>
2022-03-01 16:51:11 -0800

    Beta 0.1.0

--- pages/_document.js
+++ pages/_document.js
@@ -5,16 +5,19 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
     render() {
         return (
-        <Html>
-            <Head>
-            <link  href="https://fonts.googleapis.com/css2?family=Inter:wght@300;600&display=swap" rel="stylesheet"
-            />
-            </Head>
-            <body>
-            <Main />
-            <NextScript />
-            </body>
-        </Html>
+            <Html>
+                <Head>
+                    <meta name="description" content="TruckDriver.help предоставляем эффективный сервис по поиску работы, а также размещению вакансий на работу в траковой индустрие."></meta>
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;600&display=swap" rel="stylesheet"
+                    />
+                    <title>Работа в траковых компаниях для водителей с CDL.</title>
+                </Head>
+                <body>
+
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
         )
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ function Home({ jobs }) {
 
 
   return (
+
     <div className={classes.main}>
 
       <div className={classes.listings} >
