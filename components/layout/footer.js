11feef60f719cac1c058508dbf56707fb23d6a5b
dandavisjs <dandavisjs@protonmail.com>
2022-03-15 00:42:55 -0700

    Footer list item, and minor changes

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -8,6 +8,11 @@ function Navbar() {
             <span>© {new Date().getFullYear()} TruckDriver.help</span>
             <ul>
                 <li>
+                    <Link href="https://academy.truckdriver.help" passHref={true}>
+                        <a><i className="bi bi-file-text"></i>CDL Тесты</a>
+                    </Link>
+                </li>
+                <li>
                     <Link href="https://docs.truckdriver.help" passHref={true}>
                         <a><i className="bi bi-question-circle-fill"></i>Помощь</a>
                     </Link>
