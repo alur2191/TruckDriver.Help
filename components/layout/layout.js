c7710e3306eb05061d6ed0393e8a900f9f91ed2a
dandavisjs <dandavisjs@protonmail.com>
2022-03-28 10:47:19 -0700

    FB Pixel configured

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -6,6 +6,7 @@ import { useContext } from "react";
 import UserContext from '../../store/user-context'
 import { useSession } from 'next-auth/client'
 import classes from './layout.module.css'
+import ReactPixel from 'react-facebook-pixel';
 
 function Layout(props) {
     const [session] = useSession()
