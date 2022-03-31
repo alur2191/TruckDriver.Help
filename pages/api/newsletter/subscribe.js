d86dada2bae041ce56f9fbe970c7c33f2136e3a8
dandavisjs <dandavisjs@protonmail.com>
2022-03-31 13:40:27 -0700

    Mailchimp fix

--- pages/api/newsletter/subscribe.js
+++ pages/api/newsletter/subscribe.js
@@ -17,7 +17,6 @@ export default async (req, res) => {
             email_address: email,
             status: 'subscribed'
         });
-
         return res.status(201).json({ error: '' });
     } catch (error) {
         return res.status(500).json({ error: error.message || error.toString() });
