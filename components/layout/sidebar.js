51e894937792b559216e6e1f893ffa831e188073
Al <89033896+alur2191@users.noreply.github.com>
2023-05-29 10:04:44 -0700

    Update sidebar.js

--- components/layout/sidebar.js
+++ components/layout/sidebar.js
@@ -8,16 +8,16 @@ function Sidebar() {
         <h3>Ресурсы</h3>
         <ul>
           <h4>Учеба</h4>
-          <li><a href="https://academy.truckdriver.help/course/dispatch/0"><i className="bi bi-play-circle"></i>
-            Видео курсы</a></li>
           <li><a href="https://academy.truckdriver.help/quiz"><i className="bi bi-file-earmark-text"></i>
-            CDL тесты с переводом</a></li>
-          <h4>Ресурсы</h4>
+            CDL тесты с переводом</a>
+          </li>
           <li>
-            
               <a href="https://www.dmvhelp.app/"><i className="bi bi-phone"></i>
                 DMV Тесты</a>
           </li>
+          <li><a href="https://academy.truckdriver.help/course/dispatch/0"><i className="bi bi-play-circle"></i>
+            Видео курсы</a></li>
+          <h4>Ресурсы</h4>
           <li>
             <Link href="/resources/#mobile">
               <a><i className="bi bi-phone"></i>
@@ -41,10 +41,6 @@ function Sidebar() {
               <a><i className="bi bi-people"></i>
                 Сообщества, Школы, Мастерские, и прочие.</a>
             </Link>
-          </li>
-					<li style={{ cursor: 'pointer' }}>
-              <a href="https://t.me/NewYorkChat24"><i className="bi bi-telegram"></i>Телеграм группа - New York Chat</a>
-          </li>
           <li style={{ cursor: 'pointer' }}>
               <a href="https://t.me/truckdriverhelp"><i className="bi bi-telegram"></i>Телеграм Канал TruckDriver.help</a>
           </li>
